d06ce34232f61fdae95f4f82c83f265c15afc24b
Kev <kev162020@gmail.com>
2023-03-03 17:21:41 -0600

    Integración de modelos

--- premios_platzi_app/premios_platzi_app/settings.py
+++ premios_platzi_app/premios_platzi_app/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #DJANGO_APPS
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #LOCAL_APPS
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
